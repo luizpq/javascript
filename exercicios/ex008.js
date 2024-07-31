65023fd94e8f1a57128382234578f82ad77798c3
luizpq <luizfabiano1007@gmail.com>
2024-07-31 17:09:15 -0300

    alguns exercício sobre condições

---
+++ exercicios/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80.5
+console.log(`A velocidade do seu carro é ${vel}KM por hora`)
+if (vel > 80) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')}
+else {
+    console.log('Você NÃO ultrapassou o limite de velocidade. SEM MULTA!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança')
